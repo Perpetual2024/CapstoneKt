473ba7b02cdd6d8cf544b982ee2d1c528ff356d8
Perpetual Akinyi <you@example.com>
2025-09-03 23:25:50 +0300

    making the calculator logic

--- Calculator.kt
+++ Calculator.kt
@@ -3,5 +3,22 @@ import java.util.Scanner
 fun main(){
     val reader = Scanner(System.`in`)
 
-    
+    println("==== Simple Calculator ====")
+    print("Enter firts number: ")
+    val num1 = reader.nextDouble()
+
+    print("Enter an operator (+, -, *, /): ") 
+    val operator = reader.next()
+
+    print("Enter second number: ")
+    val num2 = reader.nextDouble()
+
+
+    val result = when (operator){
+        "+" -> num1 + num2
+        "-" -> num1 - num2
+        "*" -> num1 * num2
+        "/" -> if (num2 != 0.0) num1 / num2 else "Error: Division by zero"
+        else -> "Error: Invalid operator"
+    }
 }
